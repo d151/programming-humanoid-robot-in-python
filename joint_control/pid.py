f84150c1896b8e5f026cdbdebd5f17455d3274c1
Dimitris <dimitrisxiii@gmail.com>
2019-11-27 21:24:35 +0100

    exercise 2

--- joint_control/pid.py
+++ joint_control/pid.py
@@ -35,9 +35,9 @@ class PIDController(object):
         self.e2 = np.zeros(size)
         # ADJUST PARAMETERS BELOW
         delay = 0
-        self.Kp = 0
-        self.Ki = 0
-        self.Kd = 0
+        self.Kp = 45
+        self.Ki = 1
+        self.Kd = 0.2
         self.y = deque(np.zeros(size), maxlen=delay + 1)
 
     def set_delay(self, delay):
@@ -53,6 +53,12 @@ class PIDController(object):
         @return control signal
         '''
         # YOUR CODE HERE
+
+        e = target - sensor
+        self.u = self.u + (self.Kp + self.Ki*self.dt + self.Kd/self.dt)*e - (self.Kp+(2*self.Kd)/self.dt)*self.e1 + (self.Kd/self.dt)*self.e2 
+
+        self.e2 = self.e1
+        self.e1 = e
 
         return self.u
 
